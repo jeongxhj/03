52c4361e195bca4b596478dfdd570842cdf84f33
jeongxhj <55428319+jeongxhj@users.noreply.github.com>
2020-09-17 22:13:57 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a=0;
+	float b=0;
+	printf("enter an integer: ");
+	scanf("%d", &a);
+	printf("enter a float: ");
+	scanf("%f",&b);
+	printf("integer : %d, float : %f", a, b);
+	return 0;
+}
